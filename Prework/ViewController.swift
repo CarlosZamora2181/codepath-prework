c637bb8a577decdb1dbcd6871d38fd9564420c7b
Carlos Zamora <carlosxa18@me.com>
2022-01-20 21:26:36 -0500

    Added prework files to repository

---
+++ Prework/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Carlos Zamora on 1/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Sets the title in the Navigation Bar
+            self.title = "Tip Calculator"
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double (billAmountTextField.text!) ?? 0
+        // Get Total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill *
+        tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // Update Total Amount
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+}
+
